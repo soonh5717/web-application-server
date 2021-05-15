8e563be6cf4a7c56aac5f0b061d84c4b9916cd38
김순현/회원채널개발팀 <soonh.kim@musinsa.com>
2021-05-15 12:36:24 +0900

    [3.4.3.1] Add index.html 응답 추가

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,15 +1,20 @@
 package webserver;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.file.Files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class RequestHandler extends Thread {
+    private static final String URL_SEPARATOR = " ";
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
 
     private Socket connection;
@@ -23,14 +28,29 @@ public class RequestHandler extends Thread {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
+            BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+            String url = getUrl(br.readLine());
+
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
+            //byte[] body = "Hello World".getBytes();
+
+            log.debug("url : {}", url);
+            byte[] body = readBody(url);
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
             log.error(e.getMessage());
         }
+    }
+
+    private String getUrl(String request) {
+        log.debug("request : {}", request);
+        return request.split(URL_SEPARATOR)[1];
+    }
+
+    private byte[] readBody(String url) throws IOException {
+        return Files.readAllBytes(new File("./webapp" + url).toPath());
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
